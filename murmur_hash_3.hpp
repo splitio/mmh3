9622bef71b5126a228f6b60965c8862e14e12a01
Martin Redolatti <martinredolatti@gmail.com>
2018-03-12 14:06:07 -0300

    use standard header and bump version

--- murmur_hash_3.hpp
+++ murmur_hash_3.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "stdint.h"
+#include "pstdint.h"
 
 void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out);
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
